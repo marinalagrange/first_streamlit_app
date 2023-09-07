bde55ac4de89ac7d213741f5d81793cfd23750ce
MarinaL <marinaretief@gmail.com>
2023-09-07 18:06:16 +1200

    Update streamlit_app.py
    separate the base URL from the fruit name (which will make it easier to use a variable there)

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-# write your own comment -what does the next line do? 
+# take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays data in a table
 streamlit.dataframe(fruityvice_normalized)
